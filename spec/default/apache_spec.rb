3ee5bf42b0ec07431a44416df8ed79f3478f09df
shgtkshruch <shgtk.shruch@gmail.com>
2015-01-19 12:03:26 +0900

    Add port test apache_spec

--- spec/default/apache_spec.rb
+++ spec/default/apache_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-describe package('httpd') do
-  it { should be_installed }
-end
-
 describe service('httpd') do
   it { should be_running }
   it { should be_enabled }
 end
+
+describe port('80') do
+  it { should be_listening }
+end
+
